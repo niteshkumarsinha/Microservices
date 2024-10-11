79aea347594711591f0f1f9dd19d7ec69dd89d0b
Nitesh Kumar <nitesh111sinha@gmail.com>
2024-10-11 14:58:11 +0530

    event bus services

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -12,9 +12,9 @@ app.post('/events', (req, res) => {
     const event = req.body;
     events.push(event);
     axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => console.log(err));
-    // axios.post('http://localhost:4001/events', event).catch(err => console.log(err));
-    // axios.post('http://localhost:4002/events', event).catch(err => console.log(err));
-    // axios.post('http://localhost:4003/events', event).catch(err => console.log(err));
+    axios.post('http://comments-srv:4001/events', event).catch(err => console.log(err));
+    axios.post('http://query-srv:4002/events', event).catch(err => console.log(err));
+    axios.post('http://moderation-srv:4003/events', event).catch(err => console.log(err));
     res.send({ status: 'OK'});
 });
 
